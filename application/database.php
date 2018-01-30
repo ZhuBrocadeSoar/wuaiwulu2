1d646a29784bf3d7b3aa38bbccdb425dc24d76f7
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-01-31 01:01:14 +0800

    2 数据库测试

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'wuaiwuluDB',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'nitmaker_cn',
     // 密码
-    'password'        => '',
+    'password'        => 'nitmaker.cn',
     // 端口
     'hostport'        => '',
     // 连接dsn
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,15 @@
 <?php
 namespace app\index\controller;
 
+use think\Db;
+
 class Index
 {
     public function index($name = 'thinkphp')
     {
+        $list = Db::table('user')
+            ->select();
+        dump($list);
         return 'hello ' . $name;
     }
 }

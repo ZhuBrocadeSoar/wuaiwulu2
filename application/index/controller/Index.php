40618888d8242410e6a71b219df4e7b80bd60086
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-01-31 01:11:04 +0800

    7 数据库测试

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,7 +10,7 @@ class Index
         $list = Db::table('user')
             ->where('user_index', '1')
             ->select();
-        dump($list);
+        dump($list[0]['user_date']);
         return 'hello ' . $name;
     }
 }

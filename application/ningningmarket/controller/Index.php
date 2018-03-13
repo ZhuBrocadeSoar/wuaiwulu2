98ea85009911a1a7da8ca549e07630790ed4c430
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-13 23:23:45 +0000

    路由测试

--- application/ningningmarket/controller/Index.php
+++ application/ningningmarket/controller/Index.php
@@ -4,8 +4,13 @@ namespace app\ningningmarket\controller;
 use think\Db;
 
 class Index{
-    public function index($helloMsg = 'there is ningningmarket'){
-        $retval = json_encode(array("helloMsg" => $helloMsg, "timeStamp" => time()));
+    public function index(){
+        $retval = json_encode(array("function" => "index", "data" => "this is a default function", "timeStamp" => time()));
+        return $retval;
+    }
+
+    public function hello($msg){
+        $retval = json_encode(array("function" => "hello", "data" => array("state" => "this is your hello msg", "msg" => $msg), "timeStamp" => time()));
         return $retval;
     }
 }

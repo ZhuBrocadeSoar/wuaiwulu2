8eb90649518c72de73b3b2a8007aedcc4726dbda
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-14 16:09:52 +0000

    descTable

--- application/ningningmarket/controller/DbDebug.php
+++ application/ningningmarket/controller/DbDebug.php
@@ -8,6 +8,11 @@ class DbDebug{
         $retval = Db::query('SHOW TABLES');
         dump($retval);
     }
+
+    public function descTable($tableName){
+        $retval = Db::query('DESC `' . $tableName . '`');
+        dump($retval);
+    }
 }
 
 ?>

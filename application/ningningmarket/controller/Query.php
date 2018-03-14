1c647eea33c96c25bddcc81aa5a3d04ccd241daf
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-14 16:47:09 +0000

    解决宽字符存储问题 base64

--- application/ningningmarket/controller/Query.php
+++ application/ningningmarket/controller/Query.php
@@ -8,14 +8,16 @@ class Query{
         $row = Db::name('items')
             ->where('id', $id)
             ->select();
+        $row[0]["name"] = base64_decode($row[0]["name"]);
+        $row[0]["price"] = base64_decode($row[0]["price"]);
         dump($row);
     }
 
     public function registerItem($name, $price, $stock){
-        $datas = array('name' => $name, 
-            'price' => $price, 
+        $datas = array('name' => base64_encode($name), 
+            'price' => base64_encode($price), 
             'stock' => $stock,
-            'register_time_stamp' => time()
+            'register_time_stamp' => date('Y-m-d H:i:s', time())
         );
         $counts = Db::name('items')->insert($datas);
         dump($counts);

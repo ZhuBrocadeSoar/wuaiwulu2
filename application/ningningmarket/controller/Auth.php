b436313ce5ec1608bb7669d74149a9a9264999b7
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-15 16:36:06 +0000

    准备撰写该测试app的readme

--- application/ningningmarket/controller/Auth.php
+++ application/ningningmarket/controller/Auth.php
@@ -14,11 +14,48 @@ class Auth{
         return $retval;
     }
 
-    public static function isAdmin($hash){
+    public static function isAdmin0($hash){
+        if($hash == NULL){
+            $retval = false;
+        }else{
+            $row = Db::name('admins_0')
+                ->where('token', sha1('woyaochuliubashengjian'))
+                ->select();
+            if($row == NULL){
+                $retval = false;
+            }else{
+                $retval = true;
+            }
+        }
+        return $retval;
+    }
+
+    public static function isAdmin1($hash){
         if($hash == NULL){
             $retval = false;
         }else{
             $retval = true;
+        }
+        return $retval;
+    }
+
+    public static function isAdmin2($hash){
+        if($hash == NULL){
+            $retval = false;
+        }else{
+            $retval = true;
+        }
+        return $retval;
+    }
+
+    public static function isAdmin($hash){
+        if(Auth::isAdmin2($hash) 
+            || Auth::isAdmin1($hash)
+            || Auth::isAdmin0($hash)
+        ){
+            $retval = true;
+        }else{
+            $retval = false;
         }
         return $retval;
     }

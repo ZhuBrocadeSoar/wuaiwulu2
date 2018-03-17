5e4f573ed4f5c358d8f24ddfbfd8915874e4f783
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-17 19:10:40 +0000

    extends

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,7 +5,7 @@ use think\Db;
 use think\Request;
 use think\View;
 
-class Index{
+class Index extends \think\controller{
     public function index(){
         $this->assign('title', '吾爱吾庐');
         $this->fetch('index');

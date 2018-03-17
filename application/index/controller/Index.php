973087825d5d43a10e606cfe39de08463b7a8412
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-17 19:08:50 +0000

    $this->

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,11 +5,9 @@ use think\Db;
 use think\Request;
 use think\View;
 
-class Index
-{
+class Index{
     public function index(){
-        $view = new View();
-        $view->assign('title', '吾爱吾庐');
-        $view->fetch('index');
+        $this->assign('title', '吾爱吾庐');
+        $this->fetch('index');
     }
 }

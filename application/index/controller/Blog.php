2b49260096d9d5383ef9dd9afa7b00f60bec9feb
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-18 23:07:05 +0000

    debug

--- application/index/controller/Blog.php
+++ application/index/controller/Blog.php
@@ -7,40 +7,44 @@ use think\View;
 
 class Blog extends \think\Controller{
     public function index(){
-        $this->assign([
-            'title' => '-博客',
-            'blog_title' => '博客',
-            'blog_subtitle' => '原创、转载、分享  |  随笔、日志、文稿',
-            'subhead_1' => '置顶',
-            'post_url_1_1' => '/blog/2018-03-18/log-ofwuaiwulu2',
-            'post_title_1_1' => '[日志]-吾爱吾庐网站开发日志',
-            'post_date_1_1' => 'Sun 18 Mar 2018 13:27:05',
-            'post_description_1_1' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
-            'subhead_2' => '时间轴',
-            'post_url_2_1' => '/blog/2018-03-18/log-ofwuaiwulu2',
-            'post_title_2_1' => '[日志]-吾爱吾庐网站开发日志',
-            'post_date_2_1' => 'Sun 18 Mar 2018 13:27:05',
-            'post_description_2_1' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
-            'post_url_2_2' => '/blog/2018-03-18/log-ofwuaiwulu2',
-            'post_title_2_2' => '[日志]-吾爱吾庐网站开发日志',
-            'post_date_2_2' => 'Sun 18 Mar 2018 13:27:05',
-            'post_description_2_2' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
-            'post_url_2_3' => '/blog/2018-03-18/log-ofwuaiwulu2',
-            'post_title_2_3' => '[日志]-吾爱吾庐网站开发日志',
-            'post_date_2_3' => 'Sun 18 Mar 2018 13:27:05',
-            'post_description_2_3' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
-            'post_url_2_4' => '/blog/2018-03-18/log-ofwuaiwulu2',
-            'post_title_2_4' => '[日志]-吾爱吾庐网站开发日志',
-            'post_date_2_4' => 'Sun 18 Mar 2018 13:27:05',
-            'post_description_2_4' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
-            'post_url_2_5' => '/blog/2018-03-18/log-ofwuaiwulu2',
-            'post_title_2_5' => '[日志]-吾爱吾庐网站开发日志',
-            'post_date_2_5' => 'Sun 18 Mar 2018 13:27:05',
-            'post_description_2_5' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
-            'options_num' => 5,
-            'current_page' => 3,
-        ]);
-        return $this->fetch('blog');
+        if(!Request::stances()->isGet()){
+            $this->assign([
+                'title' => '-博客',
+                'blog_title' => '博客',
+                'blog_subtitle' => '原创、转载、分享  |  随笔、日志、文稿',
+                'subhead_1' => '置顶',
+                'post_url_1_1' => '/blog/2018-03-18/log-ofwuaiwulu2',
+                'post_title_1_1' => '[日志]-吾爱吾庐网站开发日志',
+                'post_date_1_1' => 'Sun 18 Mar 2018 13:27:05',
+                'post_description_1_1' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
+                'subhead_2' => '时间轴',
+                'post_url_2_1' => '/blog/2018-03-18/log-ofwuaiwulu2',
+                'post_title_2_1' => '[日志]-吾爱吾庐网站开发日志',
+                'post_date_2_1' => 'Sun 18 Mar 2018 13:27:05',
+                'post_description_2_1' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
+                'post_url_2_2' => '/blog/2018-03-18/log-ofwuaiwulu2',
+                'post_title_2_2' => '[日志]-吾爱吾庐网站开发日志',
+                'post_date_2_2' => 'Sun 18 Mar 2018 13:27:05',
+                'post_description_2_2' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
+                'post_url_2_3' => '/blog/2018-03-18/log-ofwuaiwulu2',
+                'post_title_2_3' => '[日志]-吾爱吾庐网站开发日志',
+                'post_date_2_3' => 'Sun 18 Mar 2018 13:27:05',
+                'post_description_2_3' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
+                'post_url_2_4' => '/blog/2018-03-18/log-ofwuaiwulu2',
+                'post_title_2_4' => '[日志]-吾爱吾庐网站开发日志',
+                'post_date_2_4' => 'Sun 18 Mar 2018 13:27:05',
+                'post_description_2_4' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
+                'post_url_2_5' => '/blog/2018-03-18/log-ofwuaiwulu2',
+                'post_title_2_5' => '[日志]-吾爱吾庐网站开发日志',
+                'post_date_2_5' => 'Sun 18 Mar 2018 13:27:05',
+                'post_description_2_5' => 'php/html/css/js这些东西总要先有个大概的认识，给自己定一个目标，比如给自己建立一个博客站，这既是对自己技术的锻炼也作为一份优秀的简历展示自己的想法和梦',
+                'options_num' => 5,
+                'current_page' => 3,
+            ]);
+            return $this->fetch('blog');
+        }else{
+            return json_encode(['title' => '-博客-test']);
+        }
     }
 
     public function art($date, $title){

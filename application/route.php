c8dc2b17924ae0a6d77f987914e1099e825cf4a5
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-18 01:05:55 +0000

    de

--- application/route.php
+++ application/route.php
@@ -17,5 +17,6 @@ return [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
     ],
+    'blog' => 'index/blog/index',
 
 ];

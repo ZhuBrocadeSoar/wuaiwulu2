a5233485650cb3bbae2b1a865eb39bf787569d4f
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-18 09:39:18 +0000

    debug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,7 +9,7 @@ class Index extends \think\Controller{
     public function index(){
         $this->assign([
             'title' => '',
-            'tagline' => '仰望星空：<br />创新源于模仿，<br />成功源于实践。',
+            'tagline' => '仰望星空：<br />创新源于模仿，成功源于实践。',
         ]);
         return $this->fetch('index');
     }

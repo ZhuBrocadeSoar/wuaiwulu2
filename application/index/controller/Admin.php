953783cc7d8f8a505f9de537efff56317c6c6a04
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-19 07:00:08 +0000

    debug

--- application/index/controller/Admin.php
+++ application/index/controller/Admin.php
@@ -15,8 +15,8 @@ class Admin extends \think\Controller{
         $mail->Host = "smtp.163.com";
         $mail->Port = 465;
         $mail->SMTPSecure = 'ssl';
-        $mail->Username = "zhujinteng2012@163.com";
-        $mail->Password = "";
+        $mail->Username = "brocadesoar@163.com";
+        $mail->Password = "zjt123";
         $mail->setFrom('zhujinteng2012@163.com');
         $mail->addAddress('1422090554@qq.com');
         $mail->Subject = '验证码';

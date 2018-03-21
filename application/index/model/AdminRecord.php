fe7c16b9e84dcca948120944f031d958be73c263
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-21 15:25:49 +0000

    debug

--- application/index/model/AdminRecord.php
+++ application/index/model/AdminRecord.php
@@ -8,11 +8,14 @@ class AdminRecord extends Model{
     protected $autoWriteTimestamp = 'datetime';
     protected $deleteTime = 'delete_time';
     public function getOverTimeAttr($value, $data){
+        /*
         if(date() - $data > (30 * 60 * 1000)){
             return true;
         }else{
             return false;
         }
+         */
+        return date() - $data;
     }
 }
 

8ba7084683d65821bdf8cd5db5387e41d9a82cd3
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-23 17:46:14 +0000

    debug

--- application/wooc/controller/Index.php
+++ application/wooc/controller/Index.php
@@ -15,7 +15,7 @@ class Index extends \think\Controller{
 
     public function video(){
         $video = file_get_contents('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4');
-        header('Content-Type: application/octet-stream');
+        header('Content-Type: video/mpeg4');
         header('Content-Transfer-Encoding: binary');
         header('Content-Length: ' . filesize('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4'));
         return $this->fetch('index');

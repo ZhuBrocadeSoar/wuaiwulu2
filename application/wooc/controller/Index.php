4685acf29104e8d079e6eb48008dd0c77f8a15ef
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-23 17:09:50 +0000

    debug

--- application/wooc/controller/Index.php
+++ application/wooc/controller/Index.php
@@ -9,6 +9,7 @@ class Index extends \think\Controller{
         $video = file_get_contents('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4');
         header('Content-Type: video/mpeg4');
         header('Content-Transfer-Encoding: binary');
+        header('Content-Length: ' . filesize('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4'));
         return $video;
     }
 }

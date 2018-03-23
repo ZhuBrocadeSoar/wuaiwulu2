933b50b94bac9418a27a8ee0a753700b2a798978
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-23 17:33:23 +0000

    debug

--- application/wooc/controller/Index.php
+++ application/wooc/controller/Index.php
@@ -12,6 +12,14 @@ class Index extends \think\Controller{
         header('Content-Length: ' . filesize('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4'));
         return $video;
     }
+
+    public function video(){
+        $video = file_get_contents('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4');
+        header('Content-Type: application/octet-stream');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Length: ' . filesize('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4'));
+        return $this->fetch('index');
+    }
 }
 
 ?>

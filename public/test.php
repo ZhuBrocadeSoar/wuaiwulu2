dd2b65c0b7e703da2e0e2aa558f336e44c55fbb3
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-23 18:12:02 +0000

    debug

--- public/test.php
+++ public/test.php
@@ -1,7 +1,7 @@
 <?php
 
         $video = file_get_contents('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4');
-        header('Content-Type: application/octet-stream');
+        header('Content-Type: video/mpeg4');
         header('Content-Transfer-Encoding: binary');
         header('Content-Length: ' . filesize('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4'));
         echo $video;

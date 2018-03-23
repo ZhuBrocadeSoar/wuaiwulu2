66225a9f5b7da8fb018a2e033d1e175773c540c3
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2018-03-23 17:23:03 +0000

    debug

--- application/wooc/controller/Index.php
+++ application/wooc/controller/Index.php
@@ -7,7 +7,7 @@ use think\Request;
 class Index extends \think\Controller{
     public function index(){
         $video = file_get_contents('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4');
-        header('Content-Type: video/mpeg4');
+        header('Content-Type: application/octet-stream');
         header('Content-Transfer-Encoding: binary');
         header('Content-Length: ' . filesize('/var/www/html/wuaiwulu2/public/images/CATBOX.mp4'));
         return $video;
